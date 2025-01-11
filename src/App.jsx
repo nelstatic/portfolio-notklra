a360188703a715ad38a37ee753a4386aab73dc34
Nelson Mensah H. <nelsonh@outlook.fr>
2025-01-11 20:17:38 +0100

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 
 //components
 import Header from "@components/Header.jsx";
