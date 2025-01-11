afae06a7e02bc3a75c3ffcbf416eea80f2b77b7e
Nelson Mensah H. <nelsonh@outlook.fr>
2025-01-11 19:47:39 +0100

    set workflows

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-kl/",
   plugins: [react()],
+  base: "/portfolio-klra/",
   resolve: {
     alias: [
       {
